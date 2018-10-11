83411b16e71077984ddc15d4406912a70c79a2f7
jjc <15090205309@163.com>
2018-10-11 18:43:02 +0800

    第3次

--- src/main/java/com/jk/controller/user.java
+++ src/main/java/com/jk/controller/user.java
@@ -1,5 +1,7 @@
 package com.jk.controller;
 
+import java.security.cert.CertPathValidatorSpi;
+
 /**
  * 项目名称：springboot
  * 创建人： 贾纪昌
@@ -9,4 +11,8 @@ package com.jk.controller;
  * 修改备注：
  */
 public class user {
+    public static void main(String[] args) {
+        System.out.println("aaaaaaaaaaaa");
+        System.out.println("bbbbbbbbbbbb");
+    }
 }
