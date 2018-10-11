e8af5d6fa404c4e9d2d10e0540b325fa8326b559
jjc <15090205309@163.com>
2018-10-11 17:34:47 +0800

    第一次

---
+++ src/main/java/com/jk/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.jk.controller;
+
+/**
+ * 项目名称：springboot
+ * 创建人： 贾纪昌
+ * 创建时间：2018/10/7  14:28
+ * 修改人：
+ * 修改时间：2018/10/7  14:28
+ * 修改备注：
+ */
+
+public class UserController {
+
+    public String hah(String id){
+      /**
+      *创建人：lenovo
+      *方法名：hah
+      *创建时间：2018/10/7 15:02
+      *修改人：
+      *修改时间：
+      *@param [id]
+      *@return java.lang.String
+      */
+
+
+
+
+        return "";
+    }
+}
